2ab07274224e3aac0ce9f434d0103ba3d3205f85
Unknown <joshuanits@gmail.com>
2018-07-23 18:27:58 +0930

    Added role perms

--- settings.py
+++ settings.py
@@ -7,35 +7,48 @@ from bot import config, mongo
 import discord
 from discord.ext import commands
 
-
-class ReactCog:
+class SettingsCog:
     def __init__(self, bot):
         self.bot = bot
+
+    def has_perms(self, user, channel):
+        if user.permissions_in(channel).administrator:
+            #return True
+            pass
+
+        user_roles = [role.id for role in user.roles]
+
+        # Get guild data
+        for x in mongo["guilds"].find({"guild_id": channel.guild.id}, {"_id": 0, "admin_roles": 1}):
+            for role_id in x["admin_roles"]:
+                if(role_id in user_roles):
+                    return True
+
+        return False
 
     async def on_ready(self):
         print("Settings cog loaded successfuly")
 
-    @commands.command(name='add')
+
+    #*******************************
+    # Add command
+    #
+    #*******************************
+
+    @commands.command(name='add', aliases=['a'] help="Adds a new keywoard-reaction to this guild. `keyword` argument must be a string, `emoji` argument must be a single emoji.")
     @commands.guild_only()
-    async def add_keyword(self, ctx, *, arg=None):
-        if arg is None:
-            await ctx.channel.send("You must specify arguments. Propper syntax is `,add keyword emoji`.")
+    async def add_keyword(self, ctx, keyword: str, emoji: str):
+        if not self.has_perms(ctx.author, ctx.channel):
+            await ctx.channel.send("You do not have permissions to run that command.")
             return
 
-        args = arg.split(' ')
-        if len(args) < 2:
-            await ctx.channel.send("You must provide two arguments. Propper syntax is `,add keyword emoji`.")
-            return
-
-        keyword = args[0]
         reaction = None
-
         valid = True
 
         # Check if the emoji is a custom emoji
-        if("<" in args[1]):
+        if("<" in emoji):
             # Get ID by splitting by :, then removing trailing >
-            parts = args[1].split(":")
+            parts = emoji.split(":")
 
             emoji_id = parts[2][:-1]
             emojis = list(ctx.guild.emojis)
@@ -44,8 +57,8 @@ class ReactCog:
             valid = False
 
             #Check if the ID is actually legit
-            for emoji in emojis: #Convert tuple of emojis to list, cast emojis to strings, join list into a string then split string by space
-                if str(emoji.id) == emoji_id:
+            for e in emojis: #Convert tuple of emojis to list, cast emojis to strings, join list into a string then split string by space
+                if str(e.id) == emoji_id:
                     valid = True
                     break
             
@@ -53,13 +66,13 @@ class ReactCog:
             
         else:
             # Check to make sure the emoji is actually an emoji. Valid unicode emojis are non ascii, and can be 2 characters long (fucking multicharacter emojis). 
-            if len(args[1]) > 2: # If there is definately more than one character 
+            if len(emoji) > 2: # If there is definately more than one character 
                 valid = False 
 
-            if len(args[1].encode('ascii', 'ignore')) != 0: # If the word contains any ascii characters
+            if len(emoji.encode('ascii', 'ignore')) != 0: # If the word contains any ascii characters
                 valid = False 
 
-            reaction = args[1]
+            reaction = emoji
 
         if not valid:
             await ctx.channel.send("Invalid emoji provided. If you think you are seeing this in error, contact the bot owner.")
@@ -68,10 +81,23 @@ class ReactCog:
         # reaction is probably valid. Find and update the guild's document
         mongo["guilds"].update_one({"guild_id": ctx.guild.id}, {"$addToSet": {"message_reacts": {"word": keyword, "reaction": reaction}}})
         await ctx.channel.send("Emoji added :)")
+    
+    @add_keyword.error
+    async def add_keyword_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.channel.send("You must specify arguments. Use `,help add` for more information.")
+        else:
+            raise(error)
 
-    @commands.command(name="list")
+    
+    #*******************************
+    # List command
+    #
+    #*******************************
+
+    @commands.command(name="list", help="Lists all the keywoard-reactions for this guild.")
     @commands.guild_only()
-    async def list_emoji(self, ctx, *, arg=None):
+    async def list_emoji(self, ctx):
         # Get reactions for guild
         guild_data = mongo["guilds"].find({"guild_id": ctx.guild.id})[0]
         
@@ -99,17 +125,115 @@ class ReactCog:
 
         await ctx.channel.send(embed = embed)
 
-    @commands.command(name="remove")
+    #*******************************
+    # Remove command
+    #
+    #*******************************
+
+    @commands.command(name="remove", aliases=['r'] help="Removes all keyword-reactions where the keyword is the supplied argument.")
     @commands.guild_only()
-    async def remove_react(self, ctx, *, arg=None):
-        # Get reactions for guild
-        if arg is None:
-            await ctx.channel.send("A keyword must be provided.")
+    async def remove_keywoard(self, ctx, *, keyword):
+        if not self.has_perms(ctx.author, ctx.channel):
+            await ctx.channel.send("You do not have permissions to run that command.")
             return
 
-        mongo["guilds"].update_one({"guild_id": ctx.guild.id}, {"$pull": {"message_reacts": {"word": arg}}})
-        await ctx.channel.send(f"Successfully removed all keywoard reactions using '{arg}'")
+        # Get reactions for guild
 
+        mongo["guilds"].update_one({"guild_id": ctx.guild.id}, {"$pull": {"message_reacts": {"word": keyword}}})
+        await ctx.channel.send(f"Successfully removed all keywoard reactions using '{keyword}'")
+
+    @remove_keywoard.error
+    async def remove_keywoard_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.channel.send("You must specify arguments. Use `,help remove` for more information.")
+        else:
+            raise(error)
+
+    #*******************************
+    # Addrole command
+    #
+    #*******************************
+
+    @commands.command(name='addrole', aliases=['addroles', 'addr'] help="Allows a role to use admin commands. @mention role to add it, multiple roles can be mentioned.")
+    @commands.guild_only()
+    async def add_role(self, ctx, role: str):
+        if not self.has_perms(ctx.author, ctx.channel):
+            await ctx.channel.send("You do not have permissions to run that command.")
+            return
+
+        for role in ctx.message.role_mentions:
+            mongo["guilds"].update_one({"guild_id": ctx.guild.id}, {"$addToSet": {"admin_roles": role.id}})
+
+        await ctx.channel.send("Succesfully added role(s)")
+
+    @add_role.error
+    async def add_role_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.channel.send("You must specify a role. Use `,help addrole` for more information.")
+        else:
+            raise(error)
+
+    #*******************************
+    # Removerole command
+    #
+    #*******************************
+
+    @commands.command(name='removerole', aliases=['removeroles', 'remover'] help="Disallows a role from using admin commands. @mention role to remove it, multiple roles can be mentioned.")
+    @commands.guild_only()
+    async def remove_role(self, ctx, role: str):
+        if not self.has_perms(ctx.author, ctx.channel):
+            await ctx.channel.send("You do not have permissions to run that command.")
+            return
+
+        for role in ctx.message.role_mentions:
+            mongo["guilds"].update_one({"guild_id": ctx.guild.id}, {"$pull": {"admin_roles": role.id}})
+
+        await ctx.channel.send("Succesfully removed role(s)")
+
+    @remove_role.error
+    async def remove_role_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.channel.send("You must specify a role. Use `,help addrole` for more information.")
+        else:
+            raise(error)
+
+    #*******************************
+    # listrole command
+    #
+    #*******************************
+
+    @commands.command(name='listrole', aliases=['listroles', 'listr'], help="Lists all roles that have permission to modify the bot.")
+    @commands.guild_only()
+    async def list_role(self, ctx):
+        if not self.has_perms(ctx.author, ctx.channel):
+            await ctx.channel.send("You do not have permissions to run that command.")
+            return
+
+        admin_role_ids = []
+        # Get guild data
+        for x in mongo["guilds"].find({"guild_id": ctx.guild.id}, {"_id": 0, "admin_roles": 1}):
+            for role_id in x["admin_roles"]:
+                admin_role_ids.append(role_id)
+
+        embed = discord.Embed(title=f"Admin roles for *{ctx.guild.name}*", color=0x53C1DE)
+
+        role_list = ""
+
+        for role in ctx.guild.roles:
+            if role.id in admin_role_ids:
+                role_list += f"{role}\n"
+
+    
+        embed.add_field(name=u"Admin:", value=role_list, inline=False)
+
+        await ctx.channel.send(embed=embed)
+
+    @list_role.error
+    async def list_role_error(self, ctx, error):
+        if isinstance(error, commands.errors.MissingRequiredArgument):
+            await ctx.channel.send("You must specify a role. Use `,help addrole` for more information.")
+        else:
+            raise(error)
 
 def setup(bot):
-    bot.add_cog(ReactCog(bot))
+    bot.add_cog(SettingsCog(bot))
