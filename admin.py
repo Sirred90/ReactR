e20653f38c499ef514992bea81e9c9e3b36ff84e
Unknown <joshuanits@gmail.com>
2018-07-26 10:35:39 +0930

    Added command to list all reactions from a specific server

--- admin.py
+++ admin.py
@@ -50,6 +50,46 @@ class AdminCog:
     async def on_guild_join(self, guild):
         await self.bot.get_channel(471833598153195531).send(f"Joined guild {guild.name} - {guild.id}")
 
+    
+    #*******************************
+    # Listserver command
+    # Lists all the reactions for a server with id 'arg'
+    #*******************************
+
+    @commands.command(name="listserver", hidden=True)
+    @commands.guild_only()
+    async def list_emoji(self, ctx, arg):
+        # Get reactions for guild
+        guild_data = mongo["guilds"].find({"guild_id": arg})[0]
+        
+        embed = discord.Embed(title=f"Reactions for *{arg}*", color=0x53C1DE)
+
+        reaction_list = ""
+
+        for value in guild_data["message_reacts"]:
+
+            # Assume reaction is unicode
+            reaction_text = value['reaction']
+            try:
+                int(value['reaction'])
+                for emoji in list(ctx.guild.emojis):
+                    if str(emoji.id) == value['reaction']:
+                        reaction_text = str(emoji)
+                        break
+
+            except Exception:
+                pass
+
+            reaction_list += f"{value['word']}: {reaction_text}\n"
+
+        if reaction_list == "":
+            reaction_list = "There are no reactions defined yet, add one with `add keyword emoji`"
+             
+        embed.add_field(name="Keywoard reactions", value=reaction_list, inline=False)
+
+        await ctx.channel.send(embed = embed)
+
+
 
 def setup(bot):
     bot.add_cog(AdminCog(bot))
